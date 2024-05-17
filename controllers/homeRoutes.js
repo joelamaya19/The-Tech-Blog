6159445cd46cfa81ed80e2f6e5cec38a771f35cb
joelamaya19 <joelamaya99@gmail.com>
2024-05-17 17:01:20 -0500

    added specific blog w/ comments

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const { Blog, Comment, User} = require('../models');
+const { Blog, Comment, User } = require('../models');
 
 router.get('/', async (req, res) => {
     try {
         const blogData = await Blog.findAll({
-            include: [{ model: User, attributes: ['name']}]
+            include: [{ model: User, attributes: ['name'] }]
         });
 
         const blogs = blogData.map((blogs) => blogs.get({ plain: true }));
@@ -19,15 +19,18 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('blogs/:id', async (req, res) => {
+router.get('/blogs/:id', async (req, res) => {
     try {
         const blogData = await Blog.findByPk(req.params.id, {
-            include: [{model: User, attributes: ['name']},{model: Comment}]
+            include: [
+                { model: User, attributes: ['name'] },
+                { model: Comment, include: [{ model: User, attributes: ['name'] }] }
+            ]
         });
 
-        const blogs = blogData.get({plain: true});
+        const blogs = blogData.get({ plain: true });
 
-        res.render('blog', {
+        res.render('blogs', {
             ...blogs,
             logged_in: req.session.logged_in
         });
