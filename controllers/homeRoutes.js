6ea9b091b4ac69acdcc89f4d9cabc2d2534b12c1
joelamaya19 <joelamaya99@gmail.com>
2024-05-17 16:50:13 -0500

    added homepage for blogs

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,19 +4,36 @@ const { Blog, Comment, User} = require('../models');
 router.get('/', async (req, res) => {
     try {
         const blogData = await Blog.findAll({
-            include: [{ model: Comment }]
+            include: [{ model: User, attributes: ['name']}]
         });
 
         const blogs = blogData.map((blogs) => blogs.get({ plain: true }));
 
-        res.render('dashboard', {
-            ...blogs,
+        res.render('homepage', {
+            blogs,
             logged_in: req.session.logged_in
         });
 
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
+
+router.get('blogs/:id', async (req, res) => {
+    try {
+        const blogData = await Blog.findByPk(req.params.id, {
+            include: [{model: User, attributes: ['name']},{model: Comment}]
+        });
+
+        const blogs = blogData.get({plain: true});
+
+        res.render('blog', {
+            ...blogs,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
