6e39870aa9a7bc7b4c6ee00520db36ed6ef5af30
joelamaya19 <joelamaya99@gmail.com>
2024-05-17 17:23:15 -0500

    added login/signup

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -39,4 +39,14 @@ router.get('/blogs/:id', async (req, res) => {
     }
 });
 
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+        res.redirect('/homepage');
+        return;
+    }
+
+    res.render('login');
+});
+
 module.exports = router;
