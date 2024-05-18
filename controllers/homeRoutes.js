3ab1f6762132abbc25dfcca697474f34eb44cd89
joelamaya19 <joelamaya99@gmail.com>
2024-05-18 00:07:55 -0500

    added withAuth in homeroutes for blog/:id

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -20,7 +20,7 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/blogs/:id', async (req, res) => {
+router.get('/blogs/:id', withAuth, async (req, res) => {
     try {
         const blogData = await Blog.findByPk(req.params.id, {
             include: [
