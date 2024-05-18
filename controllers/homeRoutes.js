d6bde8ae079041927da5d48d8d272ad670ee7062
joelamaya19 <joelamaya99@gmail.com>
2024-05-17 22:30:48 -0500

    adjust css

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -58,16 +58,6 @@ router.get('/dashboard', withAuth, async (req, res) => {
     }
 });
 
-// router.get('/blogs/create', withAuth, async (req, res) => {
-//     try {
-//         res.render('blogCreate', {
-//             logged_in:true,
-//         })
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// })
-
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
