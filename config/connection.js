a5046178fd16888401f0ad1ed46455f532a6ff9b
joelamaya19 <joelamaya99@gmail.com>
2024-05-18 23:19:06 -0500

    deployment test

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if ('postgres://techblog_38jb_user:DXdJ67dQIyIkV5m8z5FP4yqY1D8VvtX8@dpg-cp43aen79t8c73e9qrvg-a.oregon-postgres.render.com/techblog_38jb') {
-  sequelize = new Sequelize('postgres://techblog_38jb_user:DXdJ67dQIyIkV5m8z5FP4yqY1D8VvtX8@dpg-cp43aen79t8c73e9qrvg-a.oregon-postgres.render.com/techblog_38jb');
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
